c8aeb94f8dbc8b509cd90fa41296b3cbb4c644b9
Liverpoolfan92 <zhelev.burislav@gmail.com>
2022-07-06 17:29:08 +0300

    initial commit

---
+++ ProjectAPI/ProjectAPI/Data/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ProjectAPI.Data.Models
+{
+    public class User : IdentityUser
+    {
+        public Uri Image { get; set; }
+        public string Position { get; set; }
+        public int Age { get; set; }
+
+        public User()
+        {
+
+        }
+    }
+}
