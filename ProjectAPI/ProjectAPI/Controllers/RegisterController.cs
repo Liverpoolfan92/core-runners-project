1e105c5c21260f1b0269084b4f2cbf2c83343753
horhe579 <georgi.i.traykov.2018@elsys-bg.org>
2022-07-08 16:19:17 +0300

    regiseter_fix

--- ProjectAPI/ProjectAPI/Controllers/RegisterController.cs
+++ ProjectAPI/ProjectAPI/Controllers/RegisterController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ProjectAPI.Context;
 using ProjectAPI.Data.Models;
@@ -11,11 +12,13 @@ namespace ProjectAPI.Controllers
     public class RegisterController : ControllerBase
     {
         private readonly AppDbContext _DbContext;
+        private readonly UserManager<User> _userManager;
 
-          public RegisterController(AppDbContext testDBContext)
-          {
-              _DbContext = testDBContext;
-          }
+        public RegisterController(AppDbContext testDBContext, UserManager<User> userManager)
+        {
+            _DbContext = testDBContext;
+            _userManager = userManager;
+        }
 
         [HttpPost]
         public async Task<IActionResult> CreateUser(CreateUserInputModel input)
@@ -25,11 +28,11 @@ namespace ProjectAPI.Controllers
                 UserName = input.Name,
                 PasswordHash = input.Password,
                 Email = input.Email,
-                NormalizedUserName = input.Email.ToUpper()
+                //NormalizedUserName = input.Email.ToUpper()
 
             };
 
-            //var identityResult = await this._userManager.CreateAsync(user, input.Password);
+            var identityResult = await this._userManager.CreateAsync(newUser, input.Password);
 
 
             _DbContext.Users.Add(newUser);
