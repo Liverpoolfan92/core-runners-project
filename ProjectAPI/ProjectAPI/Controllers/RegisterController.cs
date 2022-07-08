3006e525124ed0663753f8b22bc96bf1fcff07a8
Ivaelo <bakaruchkata@gmail.com>
2022-07-08 12:40:41 +0300

    Register

---
+++ ProjectAPI/ProjectAPI/Controllers/RegisterController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectAPI.Context;
+using ProjectAPI.Data.Models;
+using ProjectAPI.Models;
+
+namespace ProjectAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly AppDbContext _DbContext;
+
+          public RegisterController(AppDbContext testDBContext)
+          {
+              _DbContext = testDBContext;
+          }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserInputModel input)
+        {
+            var newUser = new User(input.Name)
+            {
+                UserName = input.Name,
+                PasswordHash = input.Password,
+                Email = input.Email
+
+            };
+
+            //var identityResult = await this._userManager.CreateAsync(user, input.Password);
+
+
+            _DbContext.Users.Add(newUser);
+            _DbContext.SaveChanges();
+
+           return Ok();
+
+        }
+    }
+}
