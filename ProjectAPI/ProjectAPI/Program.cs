89137d76c3e4fc902423fdcfd9e08747f557a771
horhe579 <georgi.i.traykov.2018@elsys-bg.org>
2022-07-12 17:16:16 +0300

    Global Error Handling

--- ProjectAPI/ProjectAPI/Program.cs
+++ ProjectAPI/ProjectAPI/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using ProjectAPI.Context;
 using ProjectAPI.Data;
 using ProjectAPI.Data.Models;
+using ProjectAPI.Extensions;
 using ProjectAPI.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -43,6 +44,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseHttpsRedirection();
 
